767460c8d1a4eb54acf8d30a49a99af527b05f63
shankhadas <shankhadas@sureify.com>
2022-02-23 00:43:21 +0530

    1675. Minimize Deviation in Array

---
+++ 1675. Minimize Deviation in Array.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        priority_queue<int> pq;
+
+        int minval = INT_MAX;
+
+        for(int i : nums){
+
+            if(i%2==1) i*=2;
+
+            minval = min(minval,i);
+
+            pq.push(i);
+
+        }
+
+        int diff = INT_MAX;
+
+        while(!pq.empty() && pq.top()%2==0){
+
+            int maxval = pq.top();
+
+            pq.pop();
+
+            diff = min(diff,maxval-minval);
+
+            minval = min(minval,maxval/2);
+
+            pq.push(maxval/2);
+
+        }
+
+        return min(diff,pq.top()-minval);
+    }
+};
+
+int main()
+{
+    vector<int> num = {1,2,3,4};
+    int k = 3;
+    Solution sol;
+    int n=sol.minimumDeviation(num);
+    
+    cout << n << " ";
+    
+    cout<<endl;
+    return 0;
+}
