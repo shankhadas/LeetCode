c3aba1e3ace15756cd3bac18ee820dfa61e65026
shankhadas <shankhadas@sureify.com>
2022-02-23 01:09:06 +0530

    171. Excel Sheet Column Number

--- 171. Excel Sheet Column Number.cpp
+++ 171. Excel Sheet Column Number.cpp
@@ -6,9 +6,9 @@ class Solution {
 public:
     int titleToNumber(string columnTitle) {
         int res = 0;
-        for(int i=0; i<columnTitle.size(); i++){
+        for(auto i : columnTitle){
             res *= 26;
-            res += (columnTitle[i] - 'A' + 1);
+            res += (i - 'A' + 1);
         }
         return res;
     }
