e6ef6203e9eab521ee9088490d2f04b99ca7a22e
shankhadas <shankhadas@sureify.com>
2022-03-13 06:47:57 +0530

    387. First Unique Character in a String

---
+++ 387. First Unique Character in a String.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int arr[26] = {0};
+        for(char c : s){
+            arr[c - 'a']++;
+        }
+        for(int i=0; i<s.length(); i++){
+            if(arr[s[i] - 'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    string num = "loveleetcode";
+    Solution sol;
+    int n = sol.firstUniqChar(num);
+    
+    cout << n << " ";
+    
+    cout<<endl;
+    return 0;
+}
