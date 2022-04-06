943bf33d790e754813ed61c54e4d4cde3e1210ef
shankhadas <shankhadas@sureify.com>
2022-04-07 00:54:36 +0530

    876. Middle of the Linked List

---
+++ 876. Middle of the Linked List.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class ListNode
+{
+  public:
+    int key;        // data field
+    ListNode* next;     // pointer to the next node
+};
+ 
+/*
+    Takes a list and a data value, creates a new link with the given data
+    and pushes it onto the list's front.
+*/
+ListNode* push(ListNode* head, int key){
+    ListNode* node = new ListNode;          // allocate a new node in a heap and set its data
+    node->key = key;
+    node->next = head;              // set the `.next` pointer of the new node to point to the current first node of the list.             
+    return node;                    // return the new node, so it becomes the first node in the list
+}
+
+ListNode* constructList(vector<int> const &keys){        // Function for linked list implementation from a given set of keys
+    ListNode* head = nullptr;
+    for (int i = keys.size() - 1; i >= 0; i--){      // start from the end of the array
+        head = push(head, keys[i]);                  // update head here
+    }
+    return head;
+}
+
+void printList(ListNode* head){                 // Helper function to print a linked list
+    ListNode* ptr = head;
+    while (ptr){
+        cout << ptr->key << " -> ";
+        ptr = ptr->next;
+    }
+    cout << "nullptr" << endl;
+}
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        struct ListNode* fptr=head;
+        struct ListNode* sptr=head;
+        while(fptr != NULL && fptr->next != NULL){
+            fptr = (fptr->next)->next;
+            sptr = sptr->next;
+        }
+        return sptr;
+    }
+};
+ 
+int main()
+{
+    vector<int> keys = { 4, 3, 2, 1 };
+    ListNode* head = constructList(keys);
+    printList(head);
+
+    Solution sol;
+    ListNode* temp = sol.middleNode(head);
+    cout<<temp->key<<" ";
+    cout<<endl;
+ 
+    return 0;
+}
