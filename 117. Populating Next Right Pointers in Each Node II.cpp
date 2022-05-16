5617ce71e9cde37e3a3dde3b89d88ec1d13e2bdd
shankhadas <shankha.das12@outlook.com>
2022-05-17 01:08:23 +0530

    117. Populating Next Right Pointers in Each Node II

--- 117. Populating Next Right Pointers in Each Node II.cpp
+++ 117. Populating Next Right Pointers in Each Node II.cpp
@@ -2,29 +2,50 @@
 
 using namespace std;
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
 class Solution {
 public:
-    int countVowelStrings(int n) {
-        /*string vowels = "aeiou";
-        int ans;
-        vector<int> dp(26, 0);
+    Node* connect(Node* root){
+        if (!root)
+            return root;
+
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
         
-        dp['a' - 97] = 1;
-        dp['e' - 97] = 1;
-        dp['i' - 97] = 1;
-        dp['o' - 97] = 1;
-        dp['u' - 97] = 1;
-        
-        for(int i = 2; i <= n ; i++){
-            for(int j = 0; j < 5; j++){
-                for(int k = j + 1; k < 5; k++){
-                    dp[vowels[j] - 97] += dp[vowels[k] - 97];
-                }                
+        while (q.size() > 1){
+            Node* curr = q.front();
+            q.pop();
+            if (!curr){
+                q.push(NULL);
+                continue;
             }
+
+            curr->next = q.front();
+
+            if (curr->left)
+                q.push(curr->left);
+            if (curr->right)
+                q.push(curr->right);
         }
-        ans = accumulate(dp.begin(), dp.end(), 0);
-        return ans;*/
-        return (n+4)*(n+3)*(n+2)*(n+1) / 24;
+        return root;
     }
 };
 /*
