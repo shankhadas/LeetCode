629ce80f1a44ae8f28c978f8c4e27c31c602c6d2
shankhadas <shankha.das12@outlook.com>
2022-05-22 03:46:22 +0530

    13. Roman to Integer

--- 13. Roman to Integer.cpp
+++ 13. Roman to Integer.cpp
@@ -24,6 +24,18 @@ public:
             i--;
         }
         return num;
+
+        /*
+        int total=0;
+       unordered_map<char, int> m{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+       
+        for(int i=0;i<s.size();i++){
+            int bal=m[s[i]];
+            if(i<s.size()-1&&m[s[i]]<m[s[i+1]])bal*=(-1);
+            total+=bal;
+        }
+        return total;
+        */
     }
 };
 
