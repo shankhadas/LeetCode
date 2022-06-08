0f597de30993e9228fdb57b9970139a69c9963f5
shankhadas <shankha.das12@outlook.com>
2022-06-08 22:06:42 +0530

    1332. Remove Palindromic Subsequences

---
+++ 1332. Remove Palindromic Subsequences.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int i=0, j=s.size()-1;
+        while(i<j) {
+			//If string is not palindrome return 2
+            if(s[i]!=s[j]) return 2;
+            i++;
+            j--;
+        }
+        return 1;
+    }
+};
+
+int main()
+{
+    string s = "ababa";
+    Solution sol;
+    int n=sol.removePalindromeSub(s);
+    
+    cout << n << " ";
+    
+    cout<<endl;
+    return 0;
+}
