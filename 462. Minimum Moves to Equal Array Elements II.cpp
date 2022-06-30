0d26a4c2d06ebef2de7dbf04ca9dcc71ed67f6fe
shankhadas <shankha.das12@outlook.com>
2022-06-30 22:49:37 +0530

    462. Minimum Moves to Equal Array Elements II

--- 462. Minimum Moves to Equal Array Elements II.cpp
+++ 462. Minimum Moves to Equal Array Elements II.cpp
@@ -6,11 +6,9 @@ class Solution {
 public:
     int minMoves2(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int d = (nums[0]+nums[nums.size()-1])/2;
-        int mid = nums[nums.size()/2], ans = 0;      //storing median of the array in the mid variable
-        for(int i=0; i<nums.size(); i++)
-        {
-            ans += abs(nums[i]-mid);      //adding up the absolute difference b/w median and each element of the array
+        int mid = nums[nums.size()/2], ans = 0;      
+        for(int i=0; i<nums.size(); i++){
+            ans += abs(nums[i]-mid);      
         }
         return ans;
     }
