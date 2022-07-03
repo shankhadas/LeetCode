d354fef14aaff59ec11efdb2d18972b01088cd39
shankhadas <shankha.das12@outlook.com>
2022-07-03 21:51:50 +0530

    with logs

--- Recursion & Backtracking/L11. Subset Sum II.cpp
+++ Recursion & Backtracking/L11. Subset Sum II.cpp
@@ -1,17 +1,40 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+                            //   {1, 2, 2, 2, 3, 3}
+                            //    0  1  2  3  4  5
 class Solution {
 public:
     void findSubsets(int ind, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
         ans.push_back(ds);
+                            cout<<"Ans : ";
+                            for(int i=0; i<ans.size(); i++){
+                                for(int j=0; j<ans[i].size(); j++){
+                                    cout << ans[i][j] << " ";
+                                }
+                            cout << endl;
+                            }
+                            cout <<"--------------"<< endl<<endl;
+
         for(int i=ind; i<nums.size(); i++){
-            if(i != ind && nums[i] == nums[i-1])
+                            cout << "i : " << i << " & ind : " << ind<<endl;
+            if(i != ind && nums[i] == nums[i-1]){
+                            cout << "miss match : " << i <<" & "<< ind << endl;
                 continue;
+            }
             ds.push_back(nums[i]);
+                            cout << "ds :" ;
+                            for(auto i : ds) cout << i << " ";
+                            cout<< endl;
+                            cout << "Call : findSubsets(" <<i+1<<")"<<endl ;
             findSubsets(i+1, nums, ds, ans);
+                            cout << "Call complete: findSubsets(" <<i+1<<")"<<endl<<endl ;
+
             ds.pop_back();
+                            cout << "After ds :" ;
+                            for(auto i : ds) cout << i << " ";
+                            cout<< endl;
+                            cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
         }
     }
 
@@ -32,7 +55,7 @@ int main()
     // freopen("output.txt", "w", stdout);
     // #endif
     
-    vector<int> nums = {1, 2, 2};
+    vector<int> nums = {1, 2, 2, 2, 3, 3};
     
     Solution sol;
     vector<vector<int>> res = sol.subsetsWithDup(nums);
