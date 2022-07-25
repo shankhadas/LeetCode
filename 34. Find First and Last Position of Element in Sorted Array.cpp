07690438a9e783d78e62651c2a87683a013ae9e2
shankhadas <shankha.das12@outlook.com>
2022-07-25 23:18:25 +0530

    34. Find First and Last Position of Element in Sorted Array

--- 34. Find First and Last Position of Element in Sorted Array.cpp
+++ 34. Find First and Last Position of Element in Sorted Array.cpp
@@ -26,6 +26,20 @@ public:
             return {left, right};
         return {-1, -1};
     }
+
+    /*
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto low = lower_bound(nums.begin(),nums.end(),target);
+        auto up = upper_bound(nums.begin(),nums.end(),target);
+        
+        if(low == nums.end() || *low != target)    return {-1,-1};
+        
+        int first = low - nums.begin();
+        int last = up - nums.begin()-1;
+        
+        return {first ,last};
+    }
+    */
 };
 
 int main()
