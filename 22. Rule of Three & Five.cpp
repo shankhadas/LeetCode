7caf896ad45bff7d772c9250fdba7a0c99a95636
shankhadas <shankha.das12@outlook.com>
2022-09-11 01:52:29 +0530

    Rule of Three & Five

---
+++ 22. Rule of Three & Five.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    RULE OF THREE:
+        1. Destructor
+        2. Copy Cinstructor
+        3. Copy Assignment Operator
+
+    RULE OF FIVE: (After C++ 11)
+        1. Destructor
+        2. Copy Cinstructor
+        3. Copy Assignment Operator
+        4. Move Constructor
+        5. move Assignment Operator
+
+    If you need any one of them then most of the cases others are required.
+*/
