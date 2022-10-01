6b5b968b3da8196f673621b125db1d9ce6c1a4c4
shankhadas <shankha.das12@outlook.com>
2022-10-02 02:27:49 +0530

    Subset Sum II

--- Recursion & Backtracking/L11. Subset Sum II.cpp
+++ Recursion & Backtracking/L11. Subset Sum II.cpp
@@ -3,38 +3,22 @@
 using namespace std;
                             //   {1, 2, 2, 2, 3, 3}
                             //    0  1  2  3  4  5
+/*
+    TC: ~O((2^n)*n)
+    SC: O((2^n)*k)
+*/
 class Solution {
 public:
     void findSubsets(int ind, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
         ans.push_back(ds);
-                            cout<<"Ans : ";
-                            for(int i=0; i<ans.size(); i++){
-                                for(int j=0; j<ans[i].size(); j++){
-                                    cout << ans[i][j] << " ";
-                                }
-                            cout << endl;
-                            }
-                            cout <<"--------------"<< endl<<endl;
 
         for(int i=ind; i<nums.size(); i++){
-                            cout << "i : " << i << " & ind : " << ind<<endl;
-            if(i != ind && nums[i] == nums[i-1]){
-                            cout << "miss match : " << i <<" & "<< ind << endl;
+            if(i != ind && nums[i] == nums[i-1]){   // to avoid same values picked up from the array
                 continue;
             }
             ds.push_back(nums[i]);
-                            cout << "ds :" ;
-                            for(auto i : ds) cout << i << " ";
-                            cout<< endl;
-                            cout << "Call : findSubsets(" <<i+1<<")"<<endl ;
             findSubsets(i+1, nums, ds, ans);
-                            cout << "Call complete: findSubsets(" <<i+1<<")"<<endl<<endl ;
-
             ds.pop_back();
-                            cout << "After ds :" ;
-                            for(auto i : ds) cout << i << " ";
-                            cout<< endl;
-                            cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
         }
     }
 
