675e0ccc22b5c893b73e80ca1b98047d88142a9d
shankhadas <shankha.das12@outlook.com>
2024-01-07 21:50:54 +0530

    300. Longest Increasing Subsequence

--- 300. Longest Increasing Subsequence.cpp
+++ 300. Longest Increasing Subsequence.cpp
@@ -5,7 +5,6 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-      /*
       int i,n=nums.size(),ind;
       vector<int> v;
       v.push_back(nums[0]);
@@ -18,24 +17,23 @@ public:
         }
       }
       return v.size();
-      */
 
-      if (nums.empty()) {
-        return 0;
-      }
-
-      int n = nums.size();
-      vector<int> dp(n, 1);
-
-      for (int i = 1; i < n; ++i) {
-        for (int j = 0; j < i; ++j) {
-          if (nums[i] > nums[j]) {
-            dp[i] = max(dp[i], dp[j] + 1);
-          }
-        }
-      }
-
-      return *max_element(dp.begin(), dp.end());
+      // if (nums.empty()) {
+      //   return 0;
+      // }
+      //
+      // int n = nums.size();
+      // vector<int> dp(n, 1);
+      //
+      // for (int i = 1; i < n; ++i) {
+      //   for (int j = 0; j < i; ++j) {
+      //     if (nums[i] > nums[j]) {
+      //       dp[i] = max(dp[i], dp[j] + 1);
+      //     }
+      //   }
+      // }
+      //
+      // return *max_element(dp.begin(), dp.end());
     }
 };
 
