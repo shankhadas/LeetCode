d58557d64d8e38dbe019918f948f32a129c2cfc3
shankhadas <shankha.das12@outlook.com>
2024-01-12 22:07:58 +0530

    2385. Amount of Time for Binary Tree to Be Infected

---
+++ 2385. Amount of Time for Binary Tree to Be Infected.cpp
@@ -0,0 +1,87 @@
+//
+// Created by das on 12/1/24.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+
+private:
+
+    int maxDistance = 0;
+
+public:
+
+    int amountOfTime(TreeNode* root, int start) {
+
+        traverse(root, start);
+
+        return maxDistance;
+
+    }
+
+    int traverse(TreeNode* root, int start) {
+
+        int depth = 0;
+
+        if (root == nullptr) {
+
+            return depth;
+
+        }
+
+        int leftDepth = traverse(root->left, start);
+
+        int rightDepth = traverse(root->right, start);
+
+        if (root->val == start) {
+
+            maxDistance = max(leftDepth, rightDepth);
+
+            depth = -1;
+
+        } else if (leftDepth >= 0 && rightDepth >= 0) {
+
+            depth = max(leftDepth, rightDepth) + 1;
+
+        } else {
+
+            int distance = abs(leftDepth) + abs(rightDepth);
+
+            maxDistance = max(maxDistance, distance);
+
+            depth = min(leftDepth, rightDepth) - 1;
+
+        }
+
+        return depth;
+
+    }
+
+};
+
+// int main()
+// {
+//     vector<int> nums = {2,4,6,8,10};
+//
+//     Solution sol;
+//     int res=sol.numberOfArithmeticSlices(nums);
+//
+//     cout << res << "";
+//
+//     cout<<endl;
+//     return 0;
+// }
