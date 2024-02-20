54c391d4088a52c34b6093f0b0a2735fab0074d8
shankhadas <shankha.das12@outlook.com>
2024-02-20 22:16:31 +0530

    2108. Find First Palindromic String in the Array

---
+++ 2108. Find First Palindromic String in the Array.cpp
@@ -0,0 +1,31 @@
+//
+// Created by das on 20/2/24.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+  string firstPalindrome(vector<string>& words) {
+    for(auto i:words){
+      string s=i;  // store the string s
+      reverse(s.begin(),s.end());  //reverse it example string i=`aba` reverse it s='aba' is i and s is same then this is a palindrome
+      if(s==i)
+        return s;
+    }
+    return "";
+  }
+};
+
+int main()
+{
+  vector<string> words = {"abc","car","ada","racecar","cool"};
+  Solution sol;
+  string ans = sol.firstPalindrome(words);
+
+  cout << ans << " ";
+
+  cout<<endl;
+  return 0;
+}
