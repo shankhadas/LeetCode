32505aff9d80e64110b3c591bc6c38f02ecd17b4
shankhadas <shankha.das12@outlook.com>
2024-03-03 22:52:05 +0530

    2864. Maximum Odd Binary Number

--- 2864. Maximum Odd Binary Number.cpp
+++ 2864. Maximum Odd Binary Number.cpp
@@ -12,8 +12,7 @@ public:
         for (auto a : s) {
             if (a == '1')
                 cnt1++;
-            else if (a == '0')
-                cnt0++;
+            else if (a == '0') cnt0++;
         }
         string ans = string(cnt1 - 1, '1') + string(cnt0, '0') + '1';
         return ans;
