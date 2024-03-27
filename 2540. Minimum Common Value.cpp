2e6e16969f270518f8a3cda735eedb020bdb1422
shankhadas <shankha.das12@outlook.com>
2024-03-27 16:08:23 +0530

    2540. Minimum Common Value

---
+++ 2540. Minimum Common Value.cpp
@@ -0,0 +1,38 @@
+//
+// Created by das on 27/3/24.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int common = INT_MAX;
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                common = nums1[i];
+                break;
+            } else if (nums1[i] < nums2[j])
+                i++;
+              else
+                j++;
+        }
+
+        return common != INT_MAX ? common : -1;
+    }
+};
+
+int main() {
+    vector<int> nums1 = {1,2,3}, nums2 = {2,4};
+
+    Solution sol;
+    int res = sol.getCommon(nums1, nums2);
+
+    cout << res << " ";
+
+    cout<<endl;
+    return 0;
+}
